71acbb782a2104ecfc645c2b1550fe0304aeccd4
Adipatil7 <adyapatil00707@gmail.com>
2025-08-01 20:47:29 +0530

    Created Room Repository

---
+++ src/main/java/com/chat/repositories/RoomRepository.java
@@ -0,0 +1,11 @@
+package com.chat.repositories;
+
+import com.chat.entities.Room;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface RoomRepository extends MongoRepository<Room ,String > {
+
+    //get room using room id
+    Room findRoomById(String roomId);
+
+}
